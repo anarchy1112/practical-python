bed74a104ee5e033339f120392a31569400bbd25
anarchy1112 <deerbeer111@gmail.com>
2023-05-20 16:10:40 +0200

    first solutions

--- Solutions/1_10/mortgage.py
+++ Solutions/1_10/mortgage.py
@@ -19,6 +19,8 @@ while principal > 0:
         principal = principal - extra_payment
         total_paid = total_paid + extra_payment
 
+
+
     print(month, round(total_paid,2), round(principal, 2))
     
 print('Total paid', round(total_paid, 2))
